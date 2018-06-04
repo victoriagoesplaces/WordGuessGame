48bc707acb1063d04adee2e1a92bf46dc3de5ba4
Victoria Gonzalez <vmjgr@Victorias-MBP.attlocal.net>
2018-06-03 20:23:27 -0400

    pseudocode in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,43 @@
-//id = "movie" id="used" id="guess"id="wins" id="losses"
+//id = "movie" 
+
+// define variables
+    //array for possible words
+var movies = ["pocahontas", "mulan", "tangled", "frozen", "maleficent", "tarzan", "brave", "aladdin", "moana", "pinocchio", "hercules", 
+"zootopia", "bambi", "cinderella", "fantasia", "dumbo", "bolt", "coco", "cars", "atlantis"];
+//random selected movie 
+var selectedWord = "";
+//number of letter in movie
+var lettersInWord = [];
+//number of letters in movie
+var numberOfBlanks = 0;
+// blanks plus correctly guessed letters
+var partiallySolved =[];
+// incorrrect tries
+var wrongGuesses = [];
+// counters (id="used" id="guess"id="wins" id="losses")
+var winCount = 0;
+var loseCount = 0;
+var guessesLeft = 12;
+var correct = 0;
+
+//Random selection of movie variable
+
+
+// number of lines of word to be guessed
+
+
+//start game by user selecting any key
+//if correct show letter and display on letters guessed
+//else count against number of tries
+
+
+for (var i = 0; i < movies.length; i++) {     
+
+
+  
+    //Game- document.onkeyup = function(event) {
+        //var letter = event.key();
+        //if (letter === "") {
+            //score}
+
+//pseudocode
